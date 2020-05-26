62a25b7d57d593edca9f80a7979cc281ee35a309
Shaan <shaan8@hotmail.co.uk>
2020-05-26 16:59:13 +0100

    fixed silly error in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from distutils.core import setup
+import os
+import codecs
+from setuptools import setup
+
 setup(
   name = 'strangerqueries',         # How you named your package folder (MyLib)
   packages = ['strangerqueries'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A package to allow users to run impala and hive queries within python without leaving there enviroment and connecting to a database', # Give a short description about your library
-  long_description=read('README.rst'),
+ long_description=open('README.rst').read(),
   author = 'Shaan Aucharagram',                   # Type in your name
   author_email = 'info@shaanaucharagram.com',      # Type in your E-Mail
   url = 'https://github.com/ShaanAu/strangerqueries',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ShaanAu/strangerqueries/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ShaanAu/strangerqueries/archive/v_02.tar.gz',    # I explain this later on
   keywords = ['strangerqueries', 'hive', 'impala'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
